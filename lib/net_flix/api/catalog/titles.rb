433fd0e2a8a22e778eaf76d38da175603f89a513
Lawrence Stone <larry@symbolshift.com>
2012-08-30 10:42:58 -0700

    adding basic get details call to movie

--- lib/net_flix/api/catalog/titles.rb
+++ lib/net_flix/api/catalog/titles.rb
@@ -1,22 +1,26 @@
 module NetFlix
-  module API
-    module Catalog
-      module Titles 
+    module API
+        module Catalog
+            module Titles
 
-        class << self
+                class << self
 
-          # example Title.search(term: 'sneakers', max_results: 10)
-          def search(params = {})
-            NetFlix::Request.new(url: 'http://api.netflix.com/catalog/titles', parameters: params).send
-          end
+                    # example Title.search(term: 'sneakers', max_results: 10)
+                    def search(params = {})
+                        NetFlix::Request.new(url: 'http://api.netflix.com/catalog/titles', parameters: params).send
+                    end
 
-          def list(params = {})
-            NetFlix::Request.new(url: 'http://api.netflix.com/catalog/titles/index', parameters: params).send
-          end
+                    def list(params = {})
+                        NetFlix::Request.new(url: 'http://api.netflix.com/catalog/titles/index', parameters: params).send
+                    end
 
-        end # class methods
+                    def getDetails(url)
+                        NetFlix::Request.new(url: url).send
+                    end
 
-      end # class Titles
-    end # module Catalog 
-  end # module API
+                end # class methods
+
+            end # class Titles
+        end # module Catalog
+    end # module API
 end # module NetFlix

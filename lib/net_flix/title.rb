345810f595f2e66b867336bec5fa109ce5ccb04c
Lawrence Stone <larry@symbolshift.com>
2012-09-26 15:41:52 -0400

    setting up parser for get similars.

--- lib/net_flix/title.rb
+++ lib/net_flix/title.rb
@@ -38,7 +38,9 @@ module NetFlix
       end
 
       def getSimilars(title_id, title_type, params)
-          NetFlix::API::Catalog::Titles.getSimilars(title_id, title_type, params)
+          data = NetFlix::API::Catalog::Titles.getSimilars(title_id, title_type, params)
+          data.gsub!("similars_item", "title_index_item")
+          TitleBuilder.from_xml(data)
       end
 
     end

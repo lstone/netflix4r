e149f1849f945bff4e1529a1e51421ac27685368
Lawrence Stone <larry@symbolshift.com>
2012-11-05 20:51:24 -0800

    tracing auth stuff.

--- lib/net_flix/authenticator.rb
+++ lib/net_flix/authenticator.rb
@@ -1,74 +1,75 @@
 module NetFlix
-  class Authenticator < Valuable
+    class Authenticator < Valuable
 
-    require 'base64'
-    require 'digest/sha1'
+        require 'base64'
+        require 'digest/sha1'
 
-    has_value :request
-    #has_value :timestamp, default:
-    has_value :nonce, default: rand(1_000_000)
-    has_value :credentials
+        has_value :request
+        #has_value :timestamp, default:
+        has_value :nonce, default: rand(1_000_000)
+        has_value :credentials
 
-    def access_token
-      credentials.access_token
+        def access_token
+            credentials.access_token
+        end
+
+        def key
+            credentials.key
+        end
+
+        def secret
+            credentials.secret
+        end
+
+        def require_credentials
+            raise(ArgumentError, "You must configure your NetFlix API key and secret before using netflix4r.") unless credentials.valid?
+        end
+
+        def signature_base_string
+            add_authentication_parameters!
+            [request.http_method, encoded_url, encoded_parameters].join('&')
+        end
+
+        def signature_key
+            "#{Request.encode(secret)}&#{Request.encode(access_token)}"
+        end
+
+        def signature
+            Base64.encode64(HMAC::SHA1.digest(signature_key, signature_base_string)).chomp.gsub(/\n/, '')
+        end
+
+        def authentication_parameters
+            {
+                    'oauth_consumer_key' => key,
+                    'oauth_signature_method' => 'HMAC-SHA1',
+                    'oauth_timestamp' => Time.now.to_i,
+                    'oauth_nonce' => nonce,
+                    'oauth_version' => '1.0'
+            }
+        end
+
+        def add_authentication_parameters!
+            request.parameters = request.parameters.merge(authentication_parameters)
+        end
+
+        def add_signature!
+            sign = {'oauth_signature' => Request.encode(signature)}
+            request.parameters = request.parameters.merge(sign)
+        end
+
+        def sign!
+            require_credentials
+            add_authentication_parameters!
+            add_signature!
+        end
+
+        def encoded_parameters
+            Request.encode request.parameter_string
+        end
+
+        def encoded_url
+            puts Request.encode request.url
+            Request.encode request.url
+        end
     end
-
-    def key
-      credentials.key
-    end
-    
-    def secret
-      credentials.secret
-    end
-
-    def require_credentials
-      raise( ArgumentError, "You must configure your NetFlix API key and secret before using netflix4r.") unless credentials.valid?
-    end
-
-    def signature_base_string
-      add_authentication_parameters!
-      [request.http_method, encoded_url, encoded_parameters].join('&') 
-    end
-
-    def signature_key
-      "#{Request.encode(secret)}&#{Request.encode(access_token)}"
-    end
-
-    def signature
-      Base64.encode64(HMAC::SHA1.digest(signature_key,signature_base_string)).chomp.gsub(/\n/,'')
-    end
-
-    def authentication_parameters
-      {
-      'oauth_consumer_key' => key,
-      'oauth_signature_method' => 'HMAC-SHA1',
-      'oauth_timestamp' => Time.now.to_i,
-      'oauth_nonce' => nonce,
-      'oauth_version' => '1.0'
-      }
-    end
-
-    def add_authentication_parameters!
-      request.parameters = request.parameters.merge(authentication_parameters)
-    end
-
-    def add_signature!
-      sign = {'oauth_signature' => Request.encode(signature)}
-      request.parameters = request.parameters.merge( sign  )
-    end
-
-    def sign!
-      require_credentials
-      add_authentication_parameters!
-      add_signature!
-    end
-
-    def encoded_parameters
-      Request.encode request.parameter_string
-    end
-
-    def encoded_url
-      Request.encode request.url
-    end
-  end
 end
